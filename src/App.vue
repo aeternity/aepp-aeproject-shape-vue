d13d81ce2b24af7d23e8d3de408f3714dbf508a4
Vladdy <ivanov.vladdy@gmail.com>
2019-03-12 18:57:02 +0200

    To-do list vue.js app [WIP]. Currently detached from the smart contract

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+	<div id="app">
+		<ToDo/>
+	</div>
+</template>
+
+<script>
+    import ToDo from './components/ToDo.vue'
+
+    export default {
+        name: 'app',
+        components: {
+            ToDo
+        }
+    }
+</script>
+
+<style>
+	#app {
+		font-family: 'Avenir', Helvetica, Arial, sans-serif;
+		-webkit-font-smoothing: antialiased;
+		-moz-osx-font-smoothing: grayscale;
+		text-align: center;
+		margin: 200px;
+		color: #2c3e50;
+	}
+</style>
---
+++ src/store/toDo.module.js
@@ -0,0 +1,100 @@
+const state = {
+    publicKey: 'ak_zPoY7cSHy2wBKFsdWJGXM7LnSjVt6cn1TWBDdRBUMC7Tur2NQ',
+    privateKey: '36595b50bf097cd19423c40ee66b117ed15fc5ec03d8676796bdf32bc8fe367d82517293a0f82362eb4f93d0de77af5724fba64cbcf55542328bc173dbe13d33',
+    toDos: [
+        {
+            id: 0,
+            title: 'Task 1',
+            isCompleted: false
+        },
+        {
+            id: 1,
+            title: 'Task 2',
+            isCompleted: false
+        },
+        {
+            id: 2,
+            title: 'Task 3',
+            isCompleted: true
+        },
+        {
+            id: 3,
+            title: 'Task 4',
+            isCompleted: true
+        }
+    ]
+};
+
+const mutations = {
+    'SET_PRIVATE_KEY'(state, privateKey) {
+        state.privateKey = privateKey;
+    },
+    'SET_PUBLIC_KEY'(state, publicKey) {
+        state.publicKey = publicKey;
+    },
+    'SET_PRODUCTS'(state, toDos) {
+        state.toDos = [...toDos]
+    },
+    'TOGGLE_TASK_STATUS'(state, index) {
+        let updatedTask = state.toDos.filter(todo => todo.id === index)[0];
+        if (updatedTask) {
+            updatedTask.isCompleted = !updatedTask.isCompleted;
+            state.toDos = [
+                ...state.toDos.filter(element => element.id !== index),
+                updatedTask
+            ]
+        }
+    },
+    'REMOVE_TASK'(state, task) {
+        const index = state.toDos.indexOf(task);
+        if (index > -1) {
+            state.toDos.splice(index, 1);
+        }
+    }
+};
+
+const actions = {
+    setPrivateKey: ({ commit }, privateKey) => {
+        commit('SET_PRIVATE_KEY', privateKey);
+    },
+    setPublicKey: ({ commit }, publicKey) => {
+        commit('SET_PUBLIC_KEY', publicKey);
+    },
+    setToDos: ({ commit }, toDos) => {
+        commit("SET_TO_DOS", toDos)
+    },
+    toggleTaskStatus: ({ commit }, index) => {
+        commit("TOGGLE_TASK_STATUS", index)
+    },
+    removeTask: ({ commit }, task) => {
+        commit("REMOVE_TASK", task)
+    }
+};
+
+const getters = {
+    publicKey: state => {
+        return state.publicKey;
+    },
+    privateKey: state => {
+        return state.privateKey;
+    },
+    toDos: state => {
+        return state.toDos
+    },
+    doneToDos: state => {
+        return state.toDos.filter(todo => todo.isCompleted)
+    },
+    pendingToDos: state => {
+        return state.toDos.filter(todo => !todo.isCompleted)
+    },
+    remainingToDos: state => {
+        return state.toDos.filter(todo => !todo.isCompleted).length
+    }
+};
+
+export default {
+    state,
+    mutations,
+    actions,
+    getters
+};
