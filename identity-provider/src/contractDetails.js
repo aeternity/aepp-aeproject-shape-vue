be3c34d1b86d9806e46d75155f0c391ef95bf5ab
Vladdy <ivanov.vladdy@gmail.com>
2019-05-27 14:06:48 +0300

    Added identity provider

--- identity-provider/src/contractDetails.js
+++ identity-provider/src/contractDetails.js
@@ -1,5 +1,5 @@
 export default {
-    contractAddress: 'YOUR_DEPLOYED_CONTRACT_ADDRESS',
+    contractAddress: 'ct_HVb6d4kirgqzY1rShmzRTRwukcsXobjHcpLVD2EggoHmn6wt2',
     contractSource: 'contract ToDoManager =\n' +
         '\n' +
         '  record todo = {\n' +
