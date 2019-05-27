628854d2e60e9297c4d54eacfcceb62dc8884e81
Vladdy <ivanov.vladdy@gmail.com>
2019-05-27 16:48:46 +0300

    Update configuration files

--- identity-provider/src/components/Home.vue
+++ identity-provider/src/components/Home.vue
@@ -34,6 +34,7 @@
     // AE_SDK_MODULES is a webpack alias present in webpack.config.js
     import Wallet from '@aeternity/aepp-sdk/es/ae/wallet'
     import MemoryAccount from '@aeternity/aepp-sdk/es/account/memory'
+    import account from '../account'
 
     export default {
         data() {
@@ -52,8 +53,8 @@
 
                 // Local config
                 runningInFrame: window.parent !== window,
-                pub: 'ak_2mwRmUeYmfuW93ti9HMSUJzCk1EYcQEfikVSzgo6k2VghsWhgU', // Your public key
-                priv: 'bb9f0b01c8c9553cfbaf7ef81a50f977b1326801ebf7294d1c2cbccdedf27476e9bbf604e611b5460a3b3999e9771b6f60417d73ce7c5519e12f7e127a1225ca', // Your private key
+                pub: account.pub, // Your public key
+                priv: account.priv, // Your private key
                 client: null,
                 balance: null,
                 height: null,
